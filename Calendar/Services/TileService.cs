b853aa74095fe978509b643f04a4017ea74cdfa5
liyike <1485578188@qq.com>
2018-05-31 00:23:54 +0800

    UI update final

---
+++ Calendar/Services/TileService.cs
@@ -0,0 +1,69 @@
+﻿using Calendar.Tile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Calendar.Services
+{
+    class TileService
+    {
+        public static Windows.Data.Xml.Dom.XmlDocument CreateTiles(PrimaryTile primaryTile)
+        {
+            XDocument xDoc = new XDocument(
+                new XElement("tile", new XAttribute("version", 3),
+                    new XElement("visual",
+                        // Small Tile
+                        new XElement("binding", new XAttribute("branding", primaryTile.branding), new XAttribute("displayName", primaryTile.appName), new XAttribute("template", "TileSmall"),
+                            new XElement("image", new XAttribute("placement", "background"), new XAttribute("src", "Assets/background.jpg")),
+                            new XElement("group",
+                                new XElement("subgroup",
+                                    new XElement("text", primaryTile.time, new XAttribute("hint-style", "caption")),
+                                    new XElement("text", primaryTile.message, new XAttribute("hint-style", "captionsubtle"), new XAttribute("hint-wrap", true), new XAttribute("hint-maxLines", 3))
+                                )
+                            )
+                        ),
+
+                        // Medium Tile
+                        new XElement("binding", new XAttribute("branding", primaryTile.branding), new XAttribute("displayName", primaryTile.appName), new XAttribute("template", "TileMedium"),
+                            new XElement("image", new XAttribute("placement", "background"), new XAttribute("src", "Assets/background.jpg")),
+                            new XElement("group",
+                                new XElement("subgroup",
+                                    new XElement("text", primaryTile.time, new XAttribute("hint-style", "caption")),
+                                    new XElement("text", primaryTile.message, new XAttribute("hint-style", "captionsubtle"), new XAttribute("hint-wrap", true), new XAttribute("hint-maxLines", 3))
+                                )
+                            )
+                        ),
+
+                        // Wide Tile
+                        new XElement("binding", new XAttribute("branding", primaryTile.branding), new XAttribute("displayName", primaryTile.appName), new XAttribute("template", "TileWide"),
+                            new XElement("image", new XAttribute("placement", "background"), new XAttribute("src", "Assets/background.jpg")),
+                            new XElement("group",
+                                new XElement("subgroup",
+                                    new XElement("text", primaryTile.time, new XAttribute("hint-style", "caption")),
+                                    new XElement("text", primaryTile.message, new XAttribute("hint-style", "captionsubtle"), new XAttribute("hint-wrap", true), new XAttribute("hint-maxLines", 3))
+                                )
+                            )
+                        ),
+                        // Large Tile
+                        new XElement("binding", new XAttribute("branding", primaryTile.branding), new XAttribute("displayName", primaryTile.appName), new XAttribute("template", "TileLarge"),
+                            new XElement("image", new XAttribute("placement", "background"), new XAttribute("src", "Assets/background.jpg")),
+                            new XElement("group",
+                                new XElement("subgroup",
+                                    new XElement("text", primaryTile.time, new XAttribute("hint-style", "caption")),
+                                    new XElement("text", primaryTile.message, new XAttribute("hint-style", "captionsubtle"), new XAttribute("hint-wrap", true), new XAttribute("hint-maxLines", 3))
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+
+            Windows.Data.Xml.Dom.XmlDocument xmlDoc = new Windows.Data.Xml.Dom.XmlDocument();
+            xmlDoc.LoadXml(xDoc.ToString());
+            return xmlDoc;
+        }
+    }
+}
---
+++ Calendar/Tile/PrimaryTile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Tile
+{
+    public class PrimaryTile
+    {
+        public string time { get; set; } = "";
+        public string message { get; set; } = "";
+        public string branding { get; set; } = "name";
+        public string appName { get; set; } = "Calendar";
+
+        public PrimaryTile(string title = "", string detail = "")
+        {
+            time = title;
+            message = detail;
+        }
+    }
+}
